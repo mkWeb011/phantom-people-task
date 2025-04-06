6b696bbd09a65a5191ea1eb542cb1b241781e9e1
Mark Kostic <129342017+mkWeb011@users.noreply.github.com>
2025-04-06 15:24:53 +0200

    rewrite & sitemap update

--- phantom-people/functions.php
+++ phantom-people/functions.php
@@ -7,4 +7,20 @@ function phantom_people_theme_styles() {
     wp_enqueue_style('phantom-people-style', get_stylesheet_uri());
 }
 
+add_action('init', function() {
+    add_rewrite_rule('^people-sitemap/?$', 'index.php?people_sitemap=1', 'top');
+});
+
+add_filter('query_vars', function($vars) {
+    $vars[] = 'people_sitemap';
+    return $vars;
+});
+
+add_action('template_redirect', function() {
+    if (!get_query_var('people_sitemap')) return;
+    
+    include get_template_directory() . '/inc/sitemap.php';
+    exit;
+});
+
 add_action('wp_enqueue_scripts', 'phantom_people_theme_styles');
--- phantom-people/inc/sitemap.php
+++ phantom-people/inc/sitemap.php
@@ -1,20 +1,18 @@
 <?php
-add_action('init', function() {
-    add_rewrite_rule('^people-sitemap/?$', 'index.php?people_sitemap=1', 'top');
-});
-add_filter('query_vars', function($vars) {
-    $vars[] = 'people_sitemap';
-    return $vars;
-});
-add_action('template_redirect', function() {
-    if (!get_query_var('people_sitemap')) return;
-    header('Content-Type: application/xml; charset=utf-8');
-    $people = pd_get_people();
-    echo '<?xml version="1.0" encoding="UTF-8"?>';
-    echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-    foreach ($people as $p) {
-        echo '<url><loc>' . esc_url(home_url("/people/{$p['slug']}/")) . '</loc></url>';
-    }
-    echo '</urlset>';
-    exit;
-});
+header('Content-Type: application/xml; charset=utf-8');
+$people = pd_get_people(); // Funkcija koja vraća sve osobe iz CSV-a ili baze podataka
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+
+foreach ($people as $p) {
+    echo '<url>';
+    echo '<loc>' . esc_url(home_url("/people/{$p['slug']}/")) . '</loc>';
+    echo '<lastmod>' . date('Y-m-d') . '</lastmod>'; // Dodajemo datum poslednje izmene
+    echo '<changefreq>monthly</changefreq>'; // Možete promeniti učestalost ažuriranja
+    echo '<priority>0.8</priority>'; // Prioritet URL-a
+    echo '</url>';
+}
+
+echo '</urlset>';
+?>
